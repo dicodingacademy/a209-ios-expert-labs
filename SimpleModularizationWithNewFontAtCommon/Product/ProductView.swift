b9c25e08d9011f5b21ca08f5786b7a82f46e43d2
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2020-10-28 15:41:56 +0700

    Add Modul Common

---
+++ SimpleModularizationWithNewFontAtCommon/Product/ProductView.swift
@@ -0,0 +1,34 @@
+//
+//  ProductView.swift
+//  Product
+//
+//  Created by Gilang Ramadhan on 28/10/20.
+//
+
+import SwiftUI
+
+public struct ProductView<Destination: View> : View {
+  let action: (() -> Destination)
+
+  public init(action: @escaping (() -> Destination)) {
+    self.action = action
+  }
+
+  public var body: some View {
+    VStack {
+      Image("Dicoding", bundle: Bundle(identifier: "com.dicoding.academy.Common"))
+        .resizable()
+        .frame(width: 240, height: 240)
+
+      Text("Hi, i'm using Quicksand as Font")
+        .font(.custom("Quicksand-Regular", size: 20))
+
+      Text("Hi, i'm using framework's Open Sans as Font")
+        .font(.custom("OpenSansCondensed-LightItalic", size: 20))
+
+      NavigationLink(destination: self.action()) {
+        Text("Open Cart")
+      }
+    }.navigationBarTitle("Product")
+  }
+}
